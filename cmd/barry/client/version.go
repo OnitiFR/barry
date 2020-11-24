ab83cce574bf6f370a27b0e9c141c7344b78361e
Xfennec <xfennec@cqfd-corp.org>
2020-11-24 09:34:34 +0100

    fix client version

--- cmd/barry/client/version.go
+++ cmd/barry/client/version.go
@@ -1,7 +1,7 @@
 package client
 
-// Version of the client
-const Version = "1.1"
+// Version of the client (x.y.z format)
+const Version = "1.0.0"
 
 // ProtocolVersion implemented by this client
 const ProtocolVersion = 1
