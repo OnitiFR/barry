6dba283d99cf68399f1cf1f7557edbff2e3f3bac
Julien Xfennec <xfennec@cqfd-corp.org>
2021-06-08 19:17:03 +0200

    bump version

--- common/versions.go
+++ common/versions.go
@@ -4,7 +4,7 @@ package common
 const ClientVersion = "1.5.0"
 
 // ServerVersion of the server
-const ServerVersion = "0.14.1"
+const ServerVersion = "0.14.2"
 
 // ProtocolVersion implemented
 const ProtocolVersion = 2
