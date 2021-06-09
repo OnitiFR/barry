0edb2ba9de3f3f63c0083699d6338631c746d916
Julien Xfennec <xfennec@cqfd-corp.org>
2021-06-09 10:22:44 +0200

    bump client version (because common.APIStatus was updated)

--- common/versions.go
+++ common/versions.go
@@ -1,7 +1,7 @@
 package common
 
 // ClientVersion of the client (x.y.z format)
-const ClientVersion = "1.5.0"
+const ClientVersion = "1.5.1"
 
 // ServerVersion of the server
 const ServerVersion = "0.15.0"
